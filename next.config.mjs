48229b653131c95fbcf1e1eb8e16d8283fa04a76
Dirk <dirk_beukes@hotmail.com>
2022-11-02 14:03:37 +0200

    feat: :rocket: pwa integration
    made the app able to be installed as a pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { env } from "./src/env/server.mjs";
+import { env } from './src/env/server.mjs';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import withPWA from 'next-pwa';
+const runWithPWA = withPWA({
+    dest: 'public',
+});
 
 /**
  * Don't be scared of the generics here.
@@ -10,15 +18,15 @@ import { env } from "./src/env/server.mjs";
  * @constraint {{import('next').NextConfig}}
  */
 function defineNextConfig(config) {
-  return config;
+    return runWithPWA(config);
 }
 
 export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
 });
